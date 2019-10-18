2f15524e20c6ee2cc10d852722a4888299032575
VjosaShala <vjosa.up@gmail.com>
2019-10-18 18:29:22 +0200

    Added Home, About, Contact and Product Details

---
+++ scripts/site.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    function showImage(imgPath) {
+        var curImage = document.getElementById('');
+        
+        curImage.src = imgPath;
+        curImage.alt = imgText;
+        curImage.title = imgText;
+    }  
+});
